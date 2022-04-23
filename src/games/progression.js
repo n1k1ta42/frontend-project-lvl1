7afd46a63ede89b34c24738154e19188c36a2d97
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2022-04-23 15:40:56 +0500

    remove magic number

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,9 +7,10 @@ import showQuestion from '../utils/showQuestion.js';
 import buildProgression from '../utils/buildProgression.js';
 
 function game(name) {
+  const lastInderInProgression = constants.PROGRESSION_LENGHT - 1;
   const firstElem = getRandomInt(constants.MIN_NUMBER, constants.MAX_NUMBER);
   const step = getRandomInt(constants.MIN_NUMBER, constants.MAX_NUMBER);
-  const replacementIndex = getRandomInt(0, constants.PROGRESSION_LENGHT - 1);
+  const replacementIndex = getRandomInt(0, lastInderInProgression);
   const progression = buildProgression(firstElem, step, constants.PROGRESSION_LENGHT);
   const answer = String(progression[replacementIndex]);
   progression[replacementIndex] = '..';
